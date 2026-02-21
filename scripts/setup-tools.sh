7e8dcbe3f92f0dbd96fa6c1424e33ec47a017866
Sagar Chhabra <sagarchhabra02@gmail.com>
2026-02-21 16:01:42 +0000

    Adding installation scripts

---
+++ scripts/setup-tools.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Install docker
+curl -fsSL https://get.docker.com | sh
+
+# Install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
+kubectl version --client
+
+# Install k3d
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+
+# Validate Cluster details
+kubectl cluster-info
+
+# Create a k3d cluster
+k3d cluster create two-node-cluster --agents 2
+kubectl config use-context two-node-cluster
